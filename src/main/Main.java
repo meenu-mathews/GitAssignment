af32b59a2480275c0f1747e2dc5a2238e4950eb8
Meenu Maria Mathews <38442902+meenu-mathews@users.noreply.github.com>
2022-02-02 21:50:55 +0000

    If applied, this commit will add a new file with a new print message from Meenu's branch

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,6 @@ package main;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Adding some code");
+        System.out.println("This is added from Meenu's branch");
     }
 }
